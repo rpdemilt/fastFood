663f3f083a33e2aeb3d069cee4f2efd542845f04
josephjeon <35093706+josephjeon@users.noreply.github.com>
2018-02-16 21:43:19 -0800

    Update Dogs.java

--- Dogs.java
+++ Dogs.java
@@ -10,7 +10,7 @@ public class Dogs {
 	public void display(Breeds g) {
 		System.out.println("The " + g.getBreed() + " is a breed of " + g.getType() + ".");
 		System.out.println("Has an average lifespan of " + g.getAge() + " years, and can grow up to " + g.getWeight() + " pounds.");
-		System.out.println("The dog is " + g.getBark() + " and is " + g.getTrainability() + "to train.");
+		System.out.println("The dog is " + g.getBark() + " and is " + g.getTrainability() + " to train.");
 	}
 	
 }
